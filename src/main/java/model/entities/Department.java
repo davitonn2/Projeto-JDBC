25bd179aa9b7b46ec288be1c39c76aea34169254
davitonn2 <daclack134@gmail.com>
2025-06-12 08:41:50 -0300

    DepartmentDao implementado e testes feitos

--- src/main/java/model/entities/Department.java
+++ src/main/java/model/entities/Department.java
@@ -1,7 +1,6 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Department implements Serializable {
 
@@ -17,6 +16,7 @@ public class Department implements Serializable {
         this.id = id;
         this.name = name;
     }
+
     public Integer getId() {
         return id;
     }
@@ -34,23 +34,32 @@ public class Department implements Serializable {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Department that = (Department) o;
-        return Objects.equals(id, that.id);
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Department other = (Department) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     @Override
     public String toString() {
-        return "Department{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "Department [id=" + id + ", name=" + name + "]";
     }
 }
